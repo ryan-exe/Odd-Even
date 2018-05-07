8e5cfb929322854ac7fd51c6c2402d644a8304f7
rvoevodin <ryanvoevo@gmail.com>
2018-05-07 22:40:14 +1000

    - Updated README with an example - Added functional oddeven.py file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Odd-Even
 Enter a number and see if it's odd or even.
+
+## Example
+```$ python oddeven.py```
+
+**Output**
+```
+Enter a number: 2
+The number 2 is even.
+```
---
+++ oddeven.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+	print("The number " + str(number) + " is even.")
+else:
+	print("The number " + str(number) + " is odd.")
